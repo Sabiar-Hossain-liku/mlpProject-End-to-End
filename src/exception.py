34509153ce06e99c033b00df9b13a54160cbf9a3
Sabiar_hossain <hossaqinkabir@gmail.com>
2025-04-16 15:11:45 +0600

    completed the logger and exception

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys  
-
+import logging
 
 def error_massage_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -13,8 +13,19 @@ def error_massage_detail(error, error_detail:sys):
 
 
 class CustomException(Exception):
+    
     def __init__(self,error_message , error_detail:sys):
         super.__init__(error_message)
         self.error_message = error_massage_detail(error_message,error_detail = error_detail)
+    
     def __str__(self):
         return self.error_message
+
+if __name__ == '__main__':
+
+    try:
+        a=1/0
+    except Exception as e :
+        logging.info("Divided by zero error")
+        raise CustomException(e, sys)
+
